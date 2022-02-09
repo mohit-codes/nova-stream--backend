fee73029567019e4c0d950f144a8066e607f6518
mohit kushwaha <kushwahamohit000@gmail.com>
2022-02-09 18:50:57 +0530

    feat: extend user router

--- routers/user.router.js
+++ routers/user.router.js
@@ -4,8 +4,29 @@ const {
   loginUserAndSendCredentials,
   signupUserAndSendCredentials,
 } = require("../controllers/user.controller");
+const {
+  getAllLikedVideos,
+  addToLikedVideos,
+  removeFromLikedVideos,
+} = require("../controllers/like.controller");
+const {
+  getHistory,
+  addToHistory,
+  removeFromHistory,
+  deleteHistory,
+} = require("../controllers/history.controller");
+const authenticate = require("../middleware/authenticate");
 
 router.route("/login").post(loginUserAndSendCredentials);
 router.route("/signup").post(signupUserAndSendCredentials);
+
+router.route("/liked").get(authenticate, getAllLikedVideos);
+router.route("/liked/add/:videoId").post(authenticate, addToLikedVideos);
+router.route("/liked/remove/:videoId").put(authenticate, removeFromLikedVideos);
+
+router.route("/history").get(authenticate, getHistory);
+router.route("/history/add/:videoId").post(authenticate, addToHistory);
+router.route("/history/remove/:videoId").put(authenticate, removeFromHistory);
+router.route("/history/delete").delete(authenticate, deleteHistory);
 
 module.exports = router;
